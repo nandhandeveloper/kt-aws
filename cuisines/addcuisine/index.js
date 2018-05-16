5a2f09e14ab58402b498481113fee29b19e111e8
kaushik nandhan <nandhan.developer@gmail.com>
2018-05-15 20:07:44 -0400

    feat(kt-aws): add and update item if exists or not

--- cuisines/addcuisine/index.js
+++ cuisines/addcuisine/index.js
@@ -5,26 +5,46 @@ exports.handler = (event, context, callback) => {
 
     const currentDate =  new Date();
 
-    var params = {
-        TableName: 'kt-cuisines',
-        Item:{
-            "name": event.cuisineName,
-            "info": {
-                "image": event.info.image,
-                "foodItems": event.info.foodItems,
-                "description": event.info.description,
-            },
-            "createdAt": ''+currentDate,
-            "updatedAt": ''+currentDate
+    const params = {
+        TableName: "kt-cuisines",
+        Key: {
+          name: event.cuisineName
         }
-    };
-
-    docClient.put(params, (err, result)=> {
-        if(err){
-            callback(err);
+      };
+    
+      docClient.get(params, (err, result) => {
+        if (err) {
+          callback(err);
         } else {
-            callback(null, result);
+          if (Object.keys(result).length === 0) {
+            const addParams = {
+                TableName: 'kt-cuisines',
+                Item:{
+                    "name": event.cuisineName,
+                    "info": {
+                        "image": event.info.image,
+                        "foodItems": event.info.foodItems,
+                        "description": event.info.description,
+                    },
+                    "createdAt": ''+currentDate,
+                    "updatedAt": ''+currentDate
+                }
+            };
+            docClient.put(addParams, (err, result)=> {
+                if(err){
+                    return callback(err);
+                } else {
+                   return callback(null, result);
+                }
+            });
+
+          } else {
+            const error = {
+                code: "Found",
+                message: "Cuisine already exists with the name " + event.cuisineName
+              };
+              return context.fail(JSON.stringify(error));
+          }
         }
     });
-
 }
