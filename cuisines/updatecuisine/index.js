b9e04cb8df6dfcc41f887aa989a8d436027c9a1d
kaushik nandhan <nandhan.developer@gmail.com>
2018-05-26 15:12:25 -0400

    update(kt-aws): update cuisineUpdate func to node8

--- cuisines/updatecuisine/index.js
+++ cuisines/updatecuisine/index.js
@@ -1,47 +1,55 @@
 const AWS = require("aws-sdk");
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context, callback) => {
+exports.handler = async event => {
   const params = {
     TableName: "kt-cuisines",
     Key: {
       name: event.cuisineName
     }
   };
+  const cuisineParams = {
+    TableName: "kt-cuisines",
+    Item: {
+      name: event.cuisineName,
+      info: {
+        image: event.info.image,
+        foodItems: event.info.foodItems,
+        description: event.info.description
+      },
+      createdAt: "",
+      updatedAt: "" + new Date()
+    }
+  };
 
-  docClient.get(params, (err, result) => {
-    if (err) {
-      callback(err);
+  try {
+    const cuisineDetails = await docClient.get(params).promise();
+    if (Object.keys(cuisineDetails).length === 0) {
+      const error = {
+        code: "NotFound", 
+        message: "No cuisine Found wityh the name " + event.cuisineName
+      };
+      throw new Error(JSON.stringify(error));
     } else {
-      if (Object.keys(result).length === 0) {
-        const error = {
-          code: "NotFound",
-          message: "No Cuisine found with the name " + event.cuisineName
-        };
-        return context.fail(JSON.stringify(error));
-      } else {
-        const cuisineParams = {
-          TableName: "kt-cuisines",
-          Item: {
-            name: event.cuisineName,
-            info: {
-              image: event.info.image,
-              foodItems: event.info.foodItems,
-              description: event.info.description
-            },
-            createdAt: result.Item.createdAt,
-            updatedAt: "" + new Date()
-          }
+      cuisineParams.Item.createdAt = cuisineDetails.Item.createdAt;
+
+      const updatedCuisine = await docClient.put(cuisineParams).promise();
+
+      if (updatedCuisine) {
+        const successMessage = {
+          message: "Updated the cuisine " + event.cuisineName
         };
 
-        docClient.put(cuisineParams, (err, updatedResult) => {
-          if (err) {
-            callback(err);
-          } else {
-            return callback(null, updatedResult);
-          }
-        });
+        return successMessage;
+      } else {
+        const error = {
+          code: "InternalError",
+          message: "Something went wrong !!!"
+        };
+        throw new Error(JSON.stringify(error));
       }
     }
-  });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
