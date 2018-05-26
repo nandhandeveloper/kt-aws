a82bc6f23ac2a124ee4ca6593254d3af9504808b
kaushik nandhan <nandhan.developer@gmail.com>
2018-05-26 14:47:09 -0400

    update(kt-aws): update the deletecuisine to node8

--- cuisines/deletecuisine/index.js
+++ cuisines/deletecuisine/index.js
@@ -1,22 +1,33 @@
-const AWS = require('aws-sdk');
-var docClient = new AWS.DynamoDB.DocumentClient();
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context, callback) => {
+exports.handler = async event => {
+  const params = {
+    TableName: "kt-cuisines",
+    Key: {
+      name: event.cuisineName
+    }
+  };
 
-
-    var params = {
-        TableName: 'kt-cuisines',
-        Key:{
-            "name": event.cuisineName
-        }
-    };
-
-    docClient.delete(params, (err, result)=> {
-        if(err){
-            callback(err);
-        } else {
-            callback(null, result);
-        }
-    });
-
-}
+  try {
+    const cuisineDetails = await docClient.get(params).promise();
+    console.log(cuisineDetails);
+    if (Object.keys(cuisineDetails).length >= 1) {
+      const result = await docClient.delete(params).promise();
+      if (result) {
+        const successMessage = {
+          message: "Deleted a cuisine " + event.cuisineName + " successfully"
+        };
+        return successMessage;
+      }
+    } else {
+      const errorMessage = {
+        code: 'NotFound',
+        message: "No Cuisine Exists with the name " + event.cuisineName
+      };
+      throw new Error(JSON.stringify(errorMessage));
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
