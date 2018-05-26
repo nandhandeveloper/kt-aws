f4e1cec9dd31505fc98ab5fafb31d32d59fdab36
kaushik nandhan <nandhan.developer@gmail.com>
2018-05-26 14:07:40 -0400

    update(kt-aws): modify add cuisine func node 8

--- cuisines/addcuisine/index.js
+++ cuisines/addcuisine/index.js
@@ -1,50 +1,46 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context, callback) => {
+exports.handler = async event => {
+  const currentDate = new Date();
 
-    const currentDate =  new Date();
-
-    const params = {
-        TableName: "kt-cuisines",
-        Key: {
-          name: event.cuisineName
-        }
-      };
-    
-      docClient.get(params, (err, result) => {
-        if (err) {
-          callback(err);
-        } else {
-          if (Object.keys(result).length === 0) {
-            const addParams = {
-                TableName: 'kt-cuisines',
-                Item:{
-                    "name": event.cuisineName,
-                    "info": {
-                        "image": event.info.image,
-                        "foodItems": event.info.foodItems,
-                        "description": event.info.description,
-                    },
-                    "createdAt": ''+currentDate,
-                    "updatedAt": ''+currentDate
-                }
-            };
-            docClient.put(addParams, (err, result)=> {
-                if(err){
-                    return callback(err);
-                } else {
-                   return callback(null, result);
-                }
-            });
-
-          } else {
-            const error = {
-                code: "Found",
-                message: "Cuisine already exists with the name " + event.cuisineName
-              };
-              return context.fail(JSON.stringify(error));
+  const cuisineParams = {
+    TableName: "kt-cuisines",
+    Key: {
+      name: event.cuisineName
+    }
+  };
+  const addCuisineParams = {
+    TableName: "kt-cuisines",
+    Item: {
+      name: event.cuisineName,
+      info: {
+        image: event.info.image,
+        foodItems: event.info.foodItems,
+        description: event.info.description
+      },
+      createdAt: "" + currentDate,
+      updatedAt: "" + currentDate
+    }
+  };
+  try {
+    const cuisineDetails = await docClient.get(cuisineParams).promise();
+    if (Object.keys(cuisineDetails).length === 0) {
+      const addedCuisine = await docClient.put(addCuisineParams).promise();
+      if (addedCuisine) {
+          const success = {
+              message : 'added Cuisine '+event.cuisineName+' successfully'
           }
-        }
-    });
-}
+        return success;
+      }
+    } else {
+      const error = {
+        code: "Found",
+        message: "Cuisine already exists with the name " + event.cuisineName
+      };
+      throw new Error(JSON.stringify(error));
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
